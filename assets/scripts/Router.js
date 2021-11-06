9518976b5507b635df6d7e8a74b496c9c17334a6
‘Evan <puzonevan@gmail.com>
2021-11-05 23:16:28 -0700

    part 1 expose

--- assets/scripts/Router.js
+++ assets/scripts/Router.js
@@ -38,6 +38,7 @@ export class Router {
      * router instance using the 'this' keyword. Substitute 'home' for the variable
      * page
      */
+    this[page] = pageFunc;
   }
 
   /**
@@ -65,5 +66,23 @@ export class Router {
      *     and URL + hash to history
      *  4. Finally, call the stored function for the given page
      */
+
+    if(this[page]){
+      let hash;
+      if(page === "home"){
+        hash = "";
+      }
+      else{
+        hash = `#${page}`;
+      }
+
+      if(!statePopped && window.location.hash !== hash){
+        history.pushState(statePopped, `https://puzonevan.github.io/Lab7_Starter/${hash}`);
+        this[page]();
+      }
+    }
+    else{
+      console.log('Error');
+    }
   }
 }
